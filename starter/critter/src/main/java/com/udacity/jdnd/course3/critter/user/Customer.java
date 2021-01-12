0b4987c392cc05ae466fed0a0bc77f01dd37eb8f
Adam DeJans <adam.dejans@gmail.com>
2021-01-11 21:10:34 -0500

    Add files via upload

---
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/Customer.java
@@ -0,0 +1,69 @@
+package com.udacity.jdnd.course3.critter.user;
+
+import com.udacity.jdnd.course3.critter.pet.Pet;
+import org.hibernate.annotations.Nationalized;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "customer", catalog = "critter")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Nationalized
+    private String name;
+
+    private String phoneNumber;
+
+    @Column(length = 500)
+    private String notes;
+
+    //TODO: first two entries vs. last two entries.
+    //TODO: review targetEntity and mappedBy
+    @OneToMany(targetEntity = Pet.class, cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "customer")
+    private List<Pet> pets;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public List<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(List<Pet> pets) {
+        this.pets = pets;
+    }
+}
